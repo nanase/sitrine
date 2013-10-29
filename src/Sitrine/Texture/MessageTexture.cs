a61d398e4c13cd26114943afa92850a3ca0603f7
Tomona Nanase <nanase@users.noreply.github.com>
2013-10-30 08:04:04 +0900

    usingの順序変更

--- src/Sitrine/Texture/MessageTexture.cs
+++ src/Sitrine/Texture/MessageTexture.cs
@@ -22,12 +22,12 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-using Sitrine.Utils;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Text.RegularExpressions;
+using Sitrine.Utils;
 
 namespace Sitrine.Texture
 {
