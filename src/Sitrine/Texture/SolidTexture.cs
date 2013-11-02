203b99637da30e9d83560c4f1952385b6f85b54a
Tomona Nanase <nanase@users.noreply.github.com>
2013-11-03 00:24:12 +0900

    SolidTextureクラスを追加

---
+++ src/Sitrine/Texture/SolidTexture.cs
@@ -0,0 +1,53 @@
+﻿/* Sitrine - 2D Game Library with OpenTK */
+
+/* LICENSE - The MIT License (MIT)
+
+Copyright (c) 2013 Tomona Nanase
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+using OpenTK.Graphics.OpenGL;
+
+namespace Sitrine.Texture
+{
+    /// <summary>
+    /// 画像を使わずに単一色を表すテクスチャです。
+    /// </summary>
+    public class SolidTexture : Texture
+    {
+        #region -- Protected Methods --
+        /// <summary>
+        /// 描画処理を実行します。
+        /// </summary>
+        protected override void Execute()
+        {
+            GL.PushMatrix();
+            {
+                GL.Disable(EnableCap.Texture2D);
+                GL.Translate(this.Position.X, this.Position.Y, 0.0f);
+                GL.Scale(this.Width, this.Height, 1.0f);
+                GL.Color4(this.Color);
+                GL.Rect(0f, 0f, 1f, 1f);
+                GL.Enable(EnableCap.Texture2D);
+            }
+            GL.PopMatrix();
+        }
+        #endregion
+    }
+}
