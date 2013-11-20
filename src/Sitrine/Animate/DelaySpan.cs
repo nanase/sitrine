f297d5a503a8abbfb3548b5383142c57fbc90d79
Tomona Nanase <nanase@users.noreply.github.com>
2013-11-20 17:10:09 +0900

    DelaySpan構造体を追加

---
+++ src/Sitrine/Animate/DelaySpan.cs
@@ -0,0 +1,112 @@
+﻿/* Sitrine - 2D Game Library with OpenTK */
+
+/* LICENSE - The MIT License (MIT)
+
+Copyright (c) 2013 Tomona Nanase
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+using System;
+
+namespace Sitrine.Event
+{
+    /// <summary>
+    /// ストーリーイベントが次のイベントを実行するまでの遅延時間を格納します。
+    /// </summary>
+    public struct DelaySpan
+    {
+        #region -- Private Fields --
+        private int delayFrames;
+        private double delaySeconds;
+        #endregion
+
+        #region -- Public Properties --
+        /// <summary>
+        /// 遅延されるフレーム数を取得します。
+        /// </summary>
+        public int DelayFrames { get { return this.delayFrames; } }
+
+        /// <summary>
+        /// 遅延される秒数を取得します。
+        /// </summary>
+        public double DelaySeconds { get { return this.delaySeconds; } }
+        #endregion
+
+        #region -- Constructor --
+        /// <summary>
+        /// 遅延されるフレーム数を指定して新しい DelaySpan 構造体のインスタンスを初期化します。
+        /// </summary>
+        /// <param name="delayFrames">遅延されるフレーム数。</param>
+        public DelaySpan(int delayFrames)
+        {
+            this.delayFrames = delayFrames;
+            this.delaySeconds = 0.0;
+        }
+
+        /// <summary>
+        /// 遅延される秒数を指定して新しい DelaySpan 構造体のインスタンスを初期化します。
+        /// </summary>
+        /// <param name="delaySeconds">遅延される秒数。</param>
+        public DelaySpan(double delaySeconds)
+        {
+            this.delayFrames = 0;
+            this.delaySeconds = delaySeconds;
+        }
+        #endregion
+
+        #region -- Public Methods --
+        /// <summary>
+        /// 遅延されるフレーム数を加算します。
+        /// </summary>
+        /// <param name="frames">加算されるフレーム数。</param>
+        public void Add(int frames)
+        {
+            if (frames < 0)
+                throw new ArgumentOutOfRangeException("frames");
+
+            this.delayFrames += frames;
+        }
+
+        /// <summary>
+        /// 遅延される秒数を加算します。
+        /// </summary>
+        /// <param name="seconds">加算される秒数。</param>
+        public void Add(double seconds)
+        {
+            if (seconds < 0)
+                throw new ArgumentOutOfRangeException("seconds");
+
+            this.delaySeconds += seconds;
+        }
+
+        /// <summary>
+        /// 指定されたストーリーボードにウェイトイベントを指定します。
+        /// </summary>
+        /// <param name="story"></param>
+        public void SetDelayAction(Storyboard story)
+        {
+            if (story == null)
+                throw new ArgumentNullException("story");
+
+            story.Process.Wait(this.delaySeconds)
+                         .WaitFrame(this.delayFrames);
+        }
+        #endregion
+    }
+}
